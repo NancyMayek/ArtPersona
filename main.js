600ca66a2828abcbb60f91b21f2eb4a7e5931dd0
Nancy TA <mayekqua@gmail.com>
2023-02-17 00:37:02 -0600

    header terminado

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('#menu-btn');
 let menuOpen = false;
 
+
 menuBtn.addEventListener('click',() => {
-    if(!menuOpen){
+    if(!menuOpen ){
        menuBtn.classList.add('open');
        menuOpen=true; 
-        mostrarmenu();
+        
+        document.getElementById('links_2').style.display = "flex";
 
     }else{
         menuBtn.classList.remove('open');
         menuOpen=false;
-        quitarmenu();
+        document.getElementById('links_2').style.display = "none";
+
     }
 });
+
+
+
+const element = document.getElementById('links_2'); // replace with your element's ID
+
+function toggleDisplay() {
+  if (window.matchMedia('(min-width: 660px) and (max-width: 1150px)').matches) {
+    element.style.display = 'flex';
+  } else {
+    if (window.matchMedia('(max-width: 660px)').matches)
+    menuBtn.classList.remove('open');
+    element.style.display = 'none';
+  }
+}
+
+window.addEventListener('resize', toggleDisplay);
+
